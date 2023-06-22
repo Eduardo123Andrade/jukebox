a08fa561874c9ce5e6591b04e1dd027dcc1ae237
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-06-22 19:55:35 -0300

    Feat - start server when application start

--- backend/lib/backend/server/client.ex
+++ backend/lib/backend/server/client.ex
@@ -1,16 +1,13 @@
 defmodule Backend.Server.Client do
-  alias Backend.Jukebox.Video.Video
-  alias Backend.Server.Server
-
-  def start() do
-    GenServer.start_link(Server, [], name: :jukebox_list)
-  end
-
-  def push(%Video{} = element) do
+  def push(element) do
     GenServer.call(:jukebox_list, {:push, [element]})
   end
 
   def pop() do
     GenServer.call(:jukebox_list, :pop)
+  end
+
+  def get() do
+    GenServer.call(:jukebox_list, :get)
   end
 end
--- backend/lib/backend/server/server.ex
+++ backend/lib/backend/server/server.ex
@@ -1,5 +1,4 @@
 defmodule Backend.Server.Server do
-  alias Backend.Jukebox.Video.Video
   use GenServer
   require Logger
 
@@ -9,16 +8,25 @@ defmodule Backend.Server.Server do
     {:ok, video_list_state}
   end
 
+  def start_link(_initial_state) do
+    GenServer.start_link(__MODULE__, [], name: :jukebox_list)
+  end
+
   # SYNC
   @impl true
-  def handle_call({:push, [%Video{}] = video_list}, _from, video_list_state) do
+  def handle_call({:push, video_list}, _from, video_list_state) do
     new_stack = video_list_state ++ video_list
     {:reply, new_stack, new_stack}
   end
 
   @impl true
-  def handle_call(:pop, _from, [%Video{} = head | tail]) do
-    {:reply, head, tail}
+  def handle_call(:pop, _from, [head | tail]) do
+    data = %{head: head, tail: tail}
+    {:reply, data, tail}
+  end
+
+  def handle_call(:get, _from, video_list) do
+    {:reply, video_list, video_list}
   end
 
   @impl true
