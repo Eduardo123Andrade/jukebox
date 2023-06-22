60c5fa53e9704beffb3fd740be735edfed99da7b
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-06-22 18:35:58 -0300

    Feat - created add video controller

--- backend/lib/backend_web/controllers/add_video_controller.ex
+++ backend/lib/backend_web/controllers/add_video_controller.ex
@@ -1,13 +1,20 @@
 defmodule BackendWeb.AddVideoController do
+  alias Youtube.VideoDetail
   use BackendWeb, :controller
 
   alias Plug.Conn
+  alias Validate.ValidateUrl
 
   def add(%Conn{} = conn, params) do
-    IO.inspect(params)
+    %{"name" => name, "url" => url} = params
 
-    conn
-    |> put_status(:ok)
-    |> render("added.json")
+    with {:ok, video_id} <- ValidateUrl.call(url),
+         {:ok, video_detail} <- VideoDetail.get_youtube_video_details(video_id) do
+      video_data = Map.put(video_detail, :user_name, name)
+
+      conn
+      |> put_status(:ok)
+      |> render("added.json", video_data: video_data)
+    end
   end
 end
--- backend/lib/backend_web/views/add_video_view.ex
+++ backend/lib/backend_web/views/add_video_view.ex
@@ -1,9 +1,10 @@
 defmodule BackendWeb.AddVideoView do
   use BackendWeb, :view
 
-  def render("added.json", _params) do
+  def render("added.json", %{video_data: video_data}) do
     %{
-      message: "Video added successfully"
+      message: "Video added successfully",
+      video: video_data
     }
   end
 end
