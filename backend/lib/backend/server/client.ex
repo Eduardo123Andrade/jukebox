521cedd66439cdb4a902f8a0848a5f60e94ccecf
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-06-22 11:12:14 -0300

    Feat - created a client for gen server video list

---
+++ backend/lib/backend/server/client.ex
@@ -0,0 +1,16 @@
+defmodule Backend.Server.Client do
+  alias Jukebox.Video.Video
+  alias Backend.Server.Server
+
+  def start() do
+    GenServer.start_link(Server, [], name: :jukebox_list)
+  end
+
+  def push(%Video{} = element) do
+    GenServer.call(:jukebox_list, {:push, [element]})
+  end
+
+  def pop() do
+    GenServer.call(:jukebox_list, :pop)
+  end
+end
