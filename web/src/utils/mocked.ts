f39c908978203eee397b0e66ae5ab1db3df64e24
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-06-23 18:07:43 -0300

    Feat - created mock list

---
+++ web/src/utils/mocked.ts
@@ -0,0 +1,40 @@
+import { VideoDetail } from '@/interfaces'
+
+export const MOCKED_VIDEO_DETAILS: VideoDetail[] = [
+  {
+    title: 'ARCH ENEMY - The Eagle Flies Alone (OFFICIAL VIDEO)',
+    thumbnail: {
+      url: 'https://i.ytimg.com/vi/mjF1rmSV1dM/default.jpg',
+      width: 120,
+      height: 90,
+    },
+    author: 'Century Media Records',
+    id: 'mjF1rmSV1dM',
+    url: 'https://www.youtube.com/watch?v=mjF1rmSV1dM&ab_channel=CenturyMediaRecords',
+    userName: 'Jubba',
+  },
+  {
+    title: 'Evanescence - Going Under',
+    thumbnail: {
+      url: 'https://i.ytimg.com/vi/CdhqVtpR2ts/default.jpg',
+      width: 120,
+      height: 90,
+    },
+    author: 'EvanescenceVEVO',
+    id: 'CdhqVtpR2ts',
+    url: 'https://www.youtube.com/watch?v=CdhqVtpR2ts&ab_channel=EvanescenceVEVO',
+    userName: 'Jubba',
+  },
+  {
+    author: 'MissKilljoy1690',
+    id: 'cfI8o_hzaDk',
+    thumbnail: {
+      height: 90,
+      url: 'https://i.ytimg.com/vi/cfI8o_hzaDk/default.jpg',
+      width: 120,
+    },
+    title: 'Sonata Arctica - FullMoon',
+    url: 'https://www.youtube.com/watch?v=cfI8o_hzaDk&ab_channel=MissKilljoy1690',
+    userName: 'Jubba',
+  },
+]
