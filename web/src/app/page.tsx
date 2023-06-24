92d4d7086be9de2ea56aea306615608980021e4d
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-06-23 22:07:18 -0300

    Refactor - refactored Page component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,48 +1,15 @@
 'use client'
 
-import { Player } from '@/components'
-import { VideoDetail } from '@/interfaces'
+import { Header, Player } from '@/components'
 import { PlayerProvider } from '@/providers'
-import { ChangeEvent, useRef, useState } from 'react'
 
 export default function Home() {
-  const [inputVideoId, setInputVideoId] = useState<string>()
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [videos, setVideos] = useState<VideoDetail[]>([])
-
-  const onGetIdFromUrl = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    const { value } = target
-    setInputVideoId(value)
-  }
-
-  const addIdOnList = () => {
-    console.log('Nothing to do')
-  }
-
   return (
     <PlayerProvider>
-      <div className="pl-5 pt-10">
-        <h1 className="pb-2 text-lg font-bold">Jukebox do Pittalks</h1>
+      <div className="h-screen pl-5 pt-10">
+        <Header />
 
-        <div>
-          <input
-            ref={inputRef}
-            type="text"
-            value={inputVideoId}
-            className="mb-5 mt-2 w-[65%] rounded border border-black"
-            placeholder="Url"
-            onChange={onGetIdFromUrl}
-          />
-
-          <button
-            className="ml-5 w-24 rounded bg-blue-700"
-            onClick={addIdOnList}
-          >
-            <label className="text-white">teste</label>
-          </button>
-        </div>
-
-        <Player videos={videos} updateVideos={setVideos} />
+        <Player />
       </div>
     </PlayerProvider>
   )
