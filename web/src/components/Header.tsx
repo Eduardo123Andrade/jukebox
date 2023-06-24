1399e784f720e4e984bed04aa5918cc46bb4fb27
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2023-06-23 23:42:18 -0300

    Feature/connect with backend router (#4)
    * fix - removed useless layout
    
    * Feat - added cors plug
    
    * Feat - using cors
    
    * feat - added react query
    
    * fix - changed base url
    
    * Feat - using react query
    
    * Feat - post video on server
    
    ---------
    
    Co-authored-by: Eduardo Andrade <eduardo.andrade1111@gmail.com>

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,38 @@
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { usePostRequest } from '@/hooks/usePostRequest'
+import React, { ChangeEvent, useState } from 'react'
 
 interface HeaderProps {}
 
 type Input = ChangeEvent<HTMLInputElement>
 
+interface Variables {
+  name: string
+  url: string
+}
+interface RequestError {
+  message: string
+}
+
 export const Header: React.FC<HeaderProps> = (props) => {
-  const [url, setUrl] = useState<string>()
+  const [url, setUrl] = useState<string>('')
   const [name, setName] = useState<string>()
 
-  const inputRef = useRef<HTMLInputElement>(null)
+  const { mutate, isLoading } = usePostRequest<
+    unknown,
+    Variables,
+    RequestError
+  >('/video/add-on-list', {
+    onSuccess: () => {
+      setUrl('')
+    },
+    onError: ({ response }) => {
+      const {
+        data: { message },
+      } = response
+
+      alert(message)
+    },
+  })
 
   const onChangeName = ({ target }: Input) => {
     const { value } = target
@@ -24,17 +48,13 @@ export const Header: React.FC<HeaderProps> = (props) => {
     const [firstName, lastName] = name.split(' ')
     const formattedName = `${firstName} ${lastName ?? ''}`.trim()
 
-    console.log({
+    mutate({
       name: formattedName,
       url,
     })
   }
 
-  const disabled = !name || !url
-
-  useEffect(() => {
-    console.log({ disabled })
-  }, [disabled])
+  const disabled = !name || !url || isLoading
 
   return (
     <div>
@@ -43,18 +63,17 @@ export const Header: React.FC<HeaderProps> = (props) => {
       <div className="mb-5 flex">
         <div className="flex w-[60%] flex-col gap-5">
           <input
-            ref={inputRef}
             type="text"
-            className=" w-[30%] rounded "
+            className=" w-[30%] rounded"
             placeholder="Nome"
             onChange={onChangeName}
           />
 
           <input
-            ref={inputRef}
             type="text"
-            className=" w-[100%] rounded "
+            className="rounded"
             placeholder="Cole o link de video do youtube"
+            value={url}
             onChange={onGetIdFromUrl}
           />
         </div>
@@ -62,8 +81,7 @@ export const Header: React.FC<HeaderProps> = (props) => {
         <button
           disabled={disabled}
           className={`ml-5 w-24 self-end rounded
-           ${disabled ? 'bg-gray-600' : 'bg-blue-900'}
-           `}
+           ${disabled ? 'bg-gray-600' : 'bg-blue-900'}`}
           onClick={addIdOnList}
         >
           <label className="text-white">Adicionar</label>
