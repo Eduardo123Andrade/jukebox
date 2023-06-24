64753a0a89197e699ab6e148e1b1f332778ed569
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-06-23 23:31:07 -0300

    fix - changed base url

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from 'axios'
 
 export const API = axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3/videos"
+  baseURL: 'http://localhost:4000/api',
 })
