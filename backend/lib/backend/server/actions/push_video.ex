81316f37adf93a0acddaa634fa44c6840466478e
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-07-14 18:56:16 -0300

    Fix - removed IOs

--- backend/lib/backend/server/actions/push_video.ex
+++ backend/lib/backend/server/actions/push_video.ex
@@ -4,7 +4,6 @@ defmodule Backend.Server.Actions.PushVideo do
   def call(%JukeboxData{} = jukebox_data, video_list) do
     %{video_list: video_list_state, current_video: current_video} = jukebox_data
     new_list = video_list_state ++ video_list
-    IO.puts("aqui")
 
     new_data = %JukeboxData{
       current_video: current_video,
