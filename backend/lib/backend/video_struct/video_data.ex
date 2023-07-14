39c568bde6b6d74dd9faf2f342e9ddd6d31e9b73
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-07-14 18:07:59 -0300

    Feat - Created status to control changing video

--- backend/lib/backend/video_struct/video_data.ex
+++ backend/lib/backend/video_struct/video_data.ex
@@ -1,7 +1,7 @@
 defmodule Backend.VideoStruct.VideoData do
   alias Backend.VideoStruct.Thumbnail
 
-  @keys [author: "", thumbnail: %Thumbnail{}, title: "", video_id: ""]
+  @keys [author: "", thumbnail: %Thumbnail{}, title: "", video_id: "", status: :stopped]
 
   @derive {Jason.Encoder, @keys}
   defstruct @keys
